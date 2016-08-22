c2198c254ed6ab7f18bf2b71f0ee96f54996fd39
mysterywave <marscat39@gmail.com>
2016-08-21 17:21:59 -0700

    Took out imports

--- Forux/src/com/arinerron/forux/core/Window.java
+++ Forux/src/com/arinerron/forux/core/Window.java
@@ -7,8 +7,6 @@ import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
